e6f7db3ceac6a8fb4f9d4d95359bcb8ccfd5c2d0
Aleksei-dev <62004244+Aleksei-dev@users.noreply.github.com>
2021-04-21 06:34:25 +0300

    DataProvider from file for Contact tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -6,7 +6,10 @@ import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -17,20 +20,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validContacts() {
+  public Iterator<Object[]> validContacts() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[] {new ContactData()
-            .withFirstname("test1").withLastname("test1")
-            .withAddress("test 1 - 1").withMobile("+54321")
-            .withEmail("test1@mail.ru").withGroup("test1")});
-    list.add(new Object[] {new ContactData()
-            .withFirstname("test2").withLastname("test2")
-            .withAddress("test 2 - 2").withMobile("+54322")
-            .withEmail("test2@mail.ru").withGroup("test1")});
-    list.add(new Object[] {new ContactData()
-            .withFirstname("test3").withLastname("test3")
-            .withAddress("test 3 - 3").withMobile("+54323")
-            .withEmail("test3@mail.ru").withGroup("test1")});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[]{new ContactData()
+              .withFirstname(split[0])
+              .withLastname(split[1])
+              .withAddress(split[2])
+              .withMobile(split[3])
+              .withEmail(split[4])
+              .withGroup(split[5])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
